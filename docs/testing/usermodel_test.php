ea9a09d91ade08ac61cbff25f1b7fb48be903247
Tania <tetiana.hrunyk@gmail.com>
2019-12-18 10:23:04 +0100

    Testing of user model and task model was executed. All the test passed

---
+++ docs/testing/usermodel_test.php
@@ -0,0 +1,23 @@
+<? include_once "../src/lib/Models/User.php";
+    //_____________TESTING CODE___________________
+$user = new User();
+$user->Create("CuteCat", "Cat'sPass");   
+
+if ($user->Read("CuteCat")){     
+    print "User exists\n";
+} else {
+    print "User does not exist\n";
+}
+
+
+if ($user->Update_password("CuteCat", "MyCutePass")){   
+    print "Password changed\n";
+} else {
+    print "Password was not changed\n";
+}
+
+if ($user->Delete("CuteCat")){   
+    print "User was deleted\n";
+} else {
+    print "User was not deleted\n";
+}
--- src/lib/Models/Task.php
+++ src/lib/Models/Task.php
@@ -89,34 +89,3 @@ class Task extends AModel
         }
     }
 }
-
-
-
-//_____________TESTING CODE___________________
-//$task = new Task();
-
-/*$task_input = array("title" => "Drink water", "username" =>"Tania", "deadline" =>"2019-12-31", "notes" => null);
-if ($task->Create($task_input)){     
-    print "task was created\n";
-} else {
-    print "task was not created\n";
-} 
-
-$task_data = $task->Read("Drink water", "Tania");
-if ($task_data){     
-    print var_dump($task_data)."\n";
-} else {
-    print "task does not exist\n";
-}
-
-if ($task->Update_task("Tania", "deadline", "2019-12-30")){   
-    print "Task was modified\n";
-} else {
-    print "Task was not modified\n";
-}
-
-if ($task->Delete("Drink water", "Tania")){   
-    print "task was deleted\n";
-} else {
-    print "task was not deleted\n";
-}*/
