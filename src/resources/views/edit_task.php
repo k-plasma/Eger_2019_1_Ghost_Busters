075ff318200c6d62d122a8e7f8af16c30d5d67f8
Tania <tetiana.hrunyk@gmail.com>
2019-12-19 13:39:09 +0100

    Latest updates to functionality

--- src/resources/views/edit_task.php
+++ src/resources/views/edit_task.php
@@ -114,65 +114,7 @@ class Task extends AModel
 ?>
 
 
-<<<<<<< HEAD
 <?php include_once "fh/header.html"; ?>
-=======
-<!DOCTYPE html>
-<html>
-
-<head>
-  <!-- BASICS -->
-  <meta charset="utf-8">
-  <title>EKE - Application Lab Group 2</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="../../public/css/settings.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/css/isotope.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/css/animate.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/css/font-awesome.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/css/overwrite.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/css/flexslider.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="../../public/js/fancybox/jquery.fancybox.css" media="screen">
-  <link rel="stylesheet" href="../../public/css/bootstrap.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700|Open+Sans:300,400,600,700">
-
-  <link rel="stylesheet" href="../../public/css/style.css">
-  <!-- skin -->
-  <link rel="stylesheet" href="../../skin/default.css">
-  
-
-</head>
-
-<body>
-  <section id="header" class="appear"></section>
-  <div class="navbar navbar-fixed-top" role="navigation" data-0="line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);" data-300="line-height:60px; height:60px; background-color:rgba(5, 42, 62, 1);">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      	  <span class="fa fa-bars color-white"></span>
-        </button>
-        <div class="navbar-logo">
-          <a href="index.html"><img data-0="width:155px;" data-300=" width:120px;" src="../..//public/img/logo.png" alt=""></a>
-        </div>
-      </div>
-      <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav" data-0="margin-top:20px;" data-300="margin-top:5px;">
-          <li class="active"><a href="userhomepage.php">Home</a></li>
-          <li><a href="#section-about">About</a></li>
-          <li><a href="#section-works">Portfolio</a></li>
-          <li><a href="#section-contact">Contact</a></li>
-          <li><a href="index.php">Logout</a></li>
-        </ul>
-      </div>
-      <!--/.navbar-collapse -->
-    </div>
-  </div>
-  <section id="intro">
-    <div class="intro-content">
-      <h2>Welcome to E.K.E Task Manager!</h2>
-    </div>
-  </section>
->>>>>>> a7f83918d778a68f74e93ebfc3038f6428ed5ffa
   
   <?php
     /*FOR SOME UNKNOWN REASON THIS DOES NOT WORK
@@ -199,11 +141,7 @@ class Task extends AModel
         "deadline" => $_POST['deadline'], "notes" => $_POST['notes']);
         
         $task = new Task();
-<<<<<<< HEAD
         $task->Delete($_SESSION['title'], $_SESSION['username']);
-=======
-        $task->Delete($_POST['title'], $_SESSION['username']);
->>>>>>> a7f83918d778a68f74e93ebfc3038f6428ed5ffa
         
         if (!empty($errors)){
             foreach ($errors as $error){
@@ -223,13 +161,8 @@ class Task extends AModel
   
   <div>
     <form method = "post" action = "edit_task.php" id = "taskform"><pre>
-<<<<<<< HEAD
         <h3>Task Title:               </h3><input type = "text" name = "title" required value ="<?php echo $_SESSION['title'];?>"">
         <h3>Task Deadline(yyyy-mm-dd:)</h3><input type = "text" name = "deadline" required value ="<?php echo $_SESSION['deadline'];?>">
-=======
-        <h3>Task Title:               </h3><input type = "text" name = "title" required default =<?php echo $_SESSION['title'];?>>
-        <h3>Task Deadline(yyyy-mm-dd:)</h3><input type = "text" name = "deadline" required default =<?php echo $_SESSION['deadline'];?>>
->>>>>>> a7f83918d778a68f74e93ebfc3038f6428ed5ffa
         <h3>Notes:                    </h3>
 <textarea name = "notes" rows="4" cols="50">
 <?php echo $_SESSION['notes'];?>
@@ -239,71 +172,8 @@ class Task extends AModel
   </div>
 
 
-<<<<<<< HEAD
+
   <?php include_once "fh/footer.html"; ?>
-=======
-
-
-  <section id="footer" class="section footer">
-    <div class="container">
-      <div class="row animated opacity mar-bot20" data-andown="fadeIn" data-animation="animation">
-        <div class="col-sm-12 align-center">
-          <ul class="social-network social-circle">
-            <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
-            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
-            <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="row align-center mar-bot20">
-        <ul class="footer-menu">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About us</a></li>
-          <li><a href="#">Privacy policy</a></li>
-          <li><a href="#">Get in touch</a></li>
-        </ul>
-      </div>
-      <div class="row align-center copyright">
-        <div class="col-sm-12">
-          <p>Copyright &copy; All rights reserved</p>
-        </div>
-      </div>
-      <div class="credits">
-        
-        Designed by <a href="https://uni-eszterhazy.hu/">ApplicationLab-EKE.com</a>
-      </div>
-    </div>
-
-  </section>
-  <a href="#header" class="scrollup"><i class="fa fa-chevron-up"></i></a>
-
-  <!-- Javascript Library Files -->
-  <script src="../../../js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-  <script src="../../../js/jquery.js"></script>
-  <script src="../../../js/jquery.easing.1.3.js"></script>
-  <script src="../../../js/bootstrap.min.js"></script>
-  <script src="../../../js/jquery.isotope.min.js"></script>
-  <script src="../../../js/jquery.nicescroll.min.js"></script>
-  <script src="../../../js/fancybox/jquery.fancybox.pack.js"></script>
-  <script src="../../../js/skrollr.min.js"></script>
-  <script src="../../../js/jquery.scrollTo.min.js"></script>
-  <script src="../../../js/jquery.localScroll.min.js"></script>
-  <script src="../../../js/stellar.js"></script>
-  <script src="../../../js/jquery.appear.js"></script>
-  <script src="../../../js/jquery.flexslider-min.js"></script>
-
-  <!-- Contact Form JavaScript File -->
-  <script src="../../../contactform/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="../../../js/main.js"></script>
-
-</body>
-
-</html>
->>>>>>> a7f83918d778a68f74e93ebfc3038f6428ed5ffa
 
 
  
